29380e6afe3efb3c36b0cd3fb49cde2c32a69d2a
Colin Cross <ccross@android.com>
2012-03-06 17:41:22 -0800

    manta: add init files for manta boards
    Add init.manta.rc, init.manta.usb.rc, and ueventd.manta.rc
    
    Change-Id: I47bcd8546470a04f7c9f1403ae86e9f89f0ad63b

--- device.mk
+++ device.mk
@@ -22,6 +22,12 @@ endif
 
 PRODUCT_COPY_FILES := \
 	$(LOCAL_KERNEL):kernel \
+	device/samsung/manta/init.manta.rc:root/init.manta.rc \
+	device/samsung/manta/init.manta.usb.rc:root/init.manta.usb.rc \
+	device/samsung/manta/ueventd.manta.rc:root/ueventd.manta.rc
+
+# Init files for booting smdk5250 with a manta image
+PRODUCT_COPY_FILES += \
 	device/samsung/manta/init.smdk5250.rc:root/init.smdk5250.rc \
 	device/samsung/manta/init.smdk5250.usb.rc:root/init.smdk5250.usb.rc \
 	device/samsung/manta/ueventd.smdk5250.rc:root/ueventd.smdk5250.rc
