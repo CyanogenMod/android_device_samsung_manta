0e73e33d6a10fb83ab7db53d157d8f50a40c63fd
Jason Parker <kornyone@gmail.com>
2012-12-01 17:30:03 -0600

    Adding CM core chunks.  cm props text, cm.mk, cm.dependencies
    Added cm.mk
          cm-proprietary-blobs.txt
          cm.dependencies
    
    Change-Id: Ia134356844d290d7cab68f801f21e3a64288235b
    Modified: full_manta.mk

---
+++ cm.mk
@@ -0,0 +1,20 @@
+# Release name
+PRODUCT_RELEASE_NAME := Nexus10
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/manta/full_manta.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := manta
+PRODUCT_NAME := cm_manta
+PRODUCT_BRAND := Google
+PRODUCT_MODEL := Nexus 10
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=mantaray BUILD_FINGERPRINT=google/mantaray/manta:4.2.1/JOP40D/533553:user/release-keys PRIVATE_BUILD_DESC="mantaray-user 4.2.1 JOP40D 533553 release-keys"
