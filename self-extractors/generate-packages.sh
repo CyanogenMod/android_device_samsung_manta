212946d3ec70786b36494bc74991d4df747951db
Jean-Baptiste Queru <jbq@google.com>
2013-02-19 15:34:36 -0800

    Self-extractors for JDQ39
    Change-Id: I2b5eacc145d0e33774f94e2fd121f1233c77274c

--- self-extractors/generate-packages.sh
+++ self-extractors/generate-packages.sh
@@ -26,17 +26,18 @@
 # end jb-mr1-dev
 # start jb-mr1.1-dev
 # 551245 = JDP82
+# 573038 = JDQ39
 # end jb-mr1.1-dev
 BRANCH=jb-mr1.1-dev
-if test $BRANCH=jb-mr1-dev
+if test $BRANCH = jb-mr1-dev
 then
   ZIP=mantaray-ota-527662.zip
   BUILD=jop40c
 fi # jb-mr1-dev
-if test $BRANCH=jb-mr1.1-dev
+if test $BRANCH = jb-mr1.1-dev
 then
-  ZIP=mantaray-ota-551245
-  BUILD=jdp82
+  ZIP=mantaray-ota-573038
+  BUILD=jdq39
 fi # jb-mr1.1-dev
 ROOTDEVICE=manta
 DEVICE=manta
