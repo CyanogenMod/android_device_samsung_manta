dde17e5a6fd28b9d3b60dd0aab4ac28abd89bda6
philz-cwm6 <phytowardt@gmail.com>
2014-02-28 19:54:22 +0100

    fix conflicts with other devices
    - i9500 and others
    
    Change-Id: Ic58110bba477a9fcafc5402e050a84144175015d

--- Android.mk
+++ Android.mk
@@ -24,5 +24,8 @@ LOCAL_PATH := $(call my-dir)
 
 # if some modules are built directly from this directory (not subdirectories),
 # their rules should be written here.
+ifeq ($(TARGET_DEVICE),manta)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
+
+endif
