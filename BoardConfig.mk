451d0b15271798831ae9257de20e1660a184b569
Doug Zongker <dougz@android.com>
2014-04-01 12:35:36 -0700

    remove custom recovery UI for manta
    All its functionality is now in the default UI.
    
    Change-Id: I642072bcdc8c63f2ab9473ecae65481e132b9b8a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -77,8 +77,6 @@ BOARD_HAL_STATIC_LIBRARIES := libhealthd.manta libdumpstate.manta
 
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
 
-TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
-TARGET_RECOVERY_UI_LIB := librecovery_ui_manta
 TARGET_RECOVERY_UPDATER_LIBS += librecovery_updater_manta
 TARGET_RELEASETOOLS_EXTENSIONS := device/samsung/manta
 
