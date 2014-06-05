3ca30c5b42dfe5e10548e69ca313ae28f047df9c
Scott Warner <Tortel1210@gmail.com>
2014-06-05 13:58:51 -0400

    Update fingerprint to 4.4.3
    Change-Id: I884b673d98acaa54a1904e81eff12c69c2831489

--- cm.mk
+++ cm.mk
@@ -23,5 +23,5 @@ PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=mantaray \
-    BUILD_FINGERPRINT=google/mantaray/manta:4.4.2/KOT49H/937116:user/release-keys \
-    PRIVATE_BUILD_DESC="mantaray-user 4.4.2 KOT49H 937116 release-keys"
+    BUILD_FINGERPRINT=google/mantaray/manta:4.4.3/KTU84L/1148727:user/release-keys \
+    PRIVATE_BUILD_DESC="mantaray-user 4.4.3 KTU84L 1148727 release-keys"
