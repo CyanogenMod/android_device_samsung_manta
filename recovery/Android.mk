6c50e01ad0087915dfe97fdcb8ca6ddb261fd5a8
Tao Bao <tbao@google.com>
2015-08-07 22:12:03 -0700

    updater: Switch to C++ and fix the build.
    Change-Id: I9e95a72df763a33c5ee7f7de933f5bdd2c669603

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -5,7 +5,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES += bootable/recovery
-LOCAL_SRC_FILES := recovery_updater.c
+LOCAL_SRC_FILES := recovery_updater.cpp
 
 # should match TARGET_RECOVERY_UPDATER_LIBS set in BoardConfig.mk
 LOCAL_MODULE := librecovery_updater_manta
