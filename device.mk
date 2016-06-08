675e4c570a4aea6426efe366e533a4c210f583b0
Corinna Vinschen <xda@vinschen.de>
2016-06-08 16:23:54 +0200

    manta: Build Gello as browser
    Current manta builds will use the standard browser only since
    Gello hasn't been added yet to the build.  Change that to build
    with Gello.
    
    Change-Id: I0712a2e4b1c0f6b9143997e32e15cf99af9487c3

--- device.mk
+++ device.mk
@@ -207,6 +207,10 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES += \
     charger_res_images
 
+# Gello
+PRODUCT_PACKAGES += \
+    Gello
+
 $(call inherit-product-if-exists, hardware/samsung_slsi/exynos5/exynos5.mk)
 $(call inherit-product-if-exists, vendor/samsung_slsi/exynos5/exynos5-vendor.mk)
 $(call inherit-product-if-exists, vendor/samsung/manta/device-vendor.mk)
