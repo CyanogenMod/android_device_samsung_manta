d88e62482f3c2452137b3a52300a91fea8d0933a
Tetsuo55 <tetsugit@gmail.com>
2016-10-07 23:01:32 +0200

    device.mk: include widevine DRM libs
    Change-Id: I505d36502987e9280ae22dcc3bc40d5cf899bce8

--- device.mk
+++ device.mk
@@ -214,5 +214,6 @@ PRODUCT_PACKAGES += \
 $(call inherit-product-if-exists, hardware/samsung_slsi/exynos5/exynos5.mk)
 $(call inherit-product-if-exists, vendor/samsung_slsi/exynos5/exynos5-vendor.mk)
 $(call inherit-product-if-exists, vendor/samsung/manta/device-vendor.mk)
+$(call inherit-product-if-exists, vendor/widevine/manta/device-partial.mk)
 
 $(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4324/device-bcm.mk)
