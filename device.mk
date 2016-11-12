24c4e14ce3488ea27a0fa6d4ef19d33b91c6fd27
Tetsuo Osaka <tetsugit@gmail.com>
2016-11-12 11:47:19 +0100

    manta: Remove prebuilt kernel
    Using prebuilt kernels was depreciated in
    http://review.cyanogenmod.org/#/c/45408/
    
    Change-Id: I723f771acb3610d37deca286677375795ed5281b

--- device.mk
+++ device.mk
@@ -14,14 +14,7 @@
 # limitations under the License.
 #
 
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-LOCAL_KERNEL := device/samsung/manta/kernel
-else
-LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
-
 PRODUCT_COPY_FILES := \
-    $(LOCAL_KERNEL):kernel \
     device/samsung/manta/init.manta.rc:root/init.manta.rc \
     device/samsung/manta/init.manta.usb.rc:root/init.manta.usb.rc \
     device/samsung/manta/init.recovery.manta.rc:root/init.recovery.manta.rc \
